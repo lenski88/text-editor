f6af6b10dea68ab3113e06c0448ee473a19433c1
lenski88 <lenski88@icloud.com>
2021-09-18 22:03:17 +0300

    fix: fix bug add tag

--- text-editor-app/src/redux/reducers/notesReducer.js
+++ text-editor-app/src/redux/reducers/notesReducer.js
@@ -6,12 +6,14 @@ const notesReducer = (state = notesList, action) => {
   switch (action.type) {
     case CREATE_NOTE:
       let lastId = state[state.length - 1].id;
-      let newNote = { id: lastId + 1, note: action.note, tag: action.tag !==''? action.tag.split()[0]:'' };
+      let newNote = {
+        id: lastId + 1,
+        note: action.note,
+        tag: action.tag !== "" ? action.tag.split(" ")[0] : "",
+      };
       let newState = state;
       newState = state.slice();
       newState = [...newState, newNote];
-      console.log(newState);
-
       return newState;
 
     default:
